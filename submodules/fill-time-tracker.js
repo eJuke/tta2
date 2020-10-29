3157f9c3e363da95960ead724b72aa341873da67
Eugene Voynov <eugene.voynov@muranosoft.com>
2020-10-29 12:05:27 +0300

    Fill multiple days

--- submodules/fill-time-tracker.js
+++ submodules/fill-time-tracker.js
@@ -36,7 +36,47 @@ function addTtRecord(username, password, prevPageHtml, sharedRequestParams) {
     return loadTtPage(username, password, requestData);
 }
 
+async function fillSingleDay(
+    params,
+    initialResponse,
+) {
+
+    const requestParams = {
+        ctl00$_content$ddlProjects: params.selectedProject,
+        ctl00$_content$ddlCategories: params.category,
+        ctl00$_content$ucStartTime$ddlHour: params.startOfDay,
+        ctl00$_content$ucStartTime$ddlMinutes: 0,
+        ctl00$_content$ucEndTime$ddlHour: params.endOfDay,
+        ctl00$_content$ucEndTime$ddlMinutes: 0,
+        ctl00$_content$ddlUsers: params.userId,
+        ctl00$_content$ddlMonths: params.currentDate.month,
+        ctl00$_content$ddlYears: params.currentDate.year,
+        ctl00$_content$ddlDays: params.currentDate.startOf("day").toFormat("dd.MM.yyyy 0:00:00"),
+        ctl00$_content$tboxDescription: params.description,
+    }
+
+    const selectDayResponse = await selectDay(
+        params.username,
+        params.password,
+        initialResponse.data,
+        params.currentDate,
+        requestParams,
+    );
+
+    return addTtRecord(
+        params.username,
+        params.password,
+        selectDayResponse.data,
+        requestParams,
+    );
+}
+
 module.exports.fillTimeTracker = async (params) => {
+
+    if (DateTime.fromISO(params.endOfDay).diff(DateTime.fromISO(params.startDate), "days").days < 0) {
+
+        logErrorAndExit("Please check your range. Start date is after end date");
+    }
 
     const initialResponse = await loadTtPage(params.username, params.password);
 
@@ -53,36 +93,30 @@ module.exports.fillTimeTracker = async (params) => {
         logErrorAndExit("Unable to determine default project. Please specify that via --project option");
     }
 
-    const currentDate = DateTime.fromISO(params.startDate);
+    let currentDate = DateTime.fromISO(params.startDate);
+    const endDate = DateTime.fromISO(params.endDate);
 
-    const requestParams = {
-        ctl00$_content$ddlProjects: selectedProject,
-        ctl00$_content$ddlCategories: params.category,
-        ctl00$_content$ucStartTime$ddlHour: params.startOfDay,
-        ctl00$_content$ucStartTime$ddlMinutes: 0,
-        ctl00$_content$ucEndTime$ddlHour: params.endOfDay,
-        ctl00$_content$ucEndTime$ddlMinutes: 0,
-        ctl00$_content$ddlUsers: userId,
-        ctl00$_content$ddlMonths: currentDate.month,
-        ctl00$_content$ddlYears: currentDate.year,
-        ctl00$_content$ddlDays: currentDate.startOf("day").toFormat("dd.MM.yyyy 0:00:00"),
-        ctl00$_content$tboxDescription: params.description,
-    }
+    do {
 
-    const selectDayResponse = await selectDay(
-        params.username,
-        params.password,
-        initialResponse.data,
-        currentDate,
-        requestParams,
-    );
+        try {
 
-    const newTtRecordResponse = await addTtRecord(
-        params.username,
-        params.password,
-        selectDayResponse.data,
-        requestParams,
-    );
+            await fillSingleDay(
+                {
+                    ...params,
+                    userId,
+                    selectedProject,
+                    currentDate,
+                },
+                initialResponse,
+            );
 
-    console.log(`${currentDate.toISODate()} - ${categories.find(i => i.value == params.category).name} - OK`);
+            console.log(`${currentDate.toISODate()} - ${categories.find(i => i.value == params.category).name} - OK`);
+        }
+        catch {
+
+            console.log(`${currentDate.toISODate()} - ${categories.find(i => i.value == params.category).name} - FAILED`);
+        }
+
+        currentDate = currentDate.plus({ day: 1 });
+    } while (endDate.diff(currentDate, "days").days >= 0)
 }
